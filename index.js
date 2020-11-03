ec5b18c79935dca16b1ff8652be6d81906dd0fcf
mariavampiro <72162944+mariavampiro@users.noreply.github.com>
2020-11-03 00:31:21 -0300

    new style and changes

---
+++ index.js
@@ -0,0 +1,97 @@
+const form = document.getElementById('form');
+const input = document.querySelectorAll('#form input');
+
+const expressions = {
+    fullname: /^[a-zA-ZÀ-ÿ\s]{6,40}$/,
+    email: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+    password: /^[a-zA-Z0-9]{8,20}$/,
+    age: /^\d{2}$/,
+    phone: /^\d{7,15}$/,
+    address: /^[a-zA-Z0-9-ÿ\s]{1,40}$/,
+    city: /^[a-zA-Z0-9]{3,20}$/,
+    zipcode: /^[a-zA-Z0-9]{3,20}$/,
+    dni: /^\d{7,8}$/,
+}
+
+const fields = {
+    fullname: false,
+    email: false,
+    password: false,
+    age: false,
+    phone: false,
+    address: false,
+    city: false,
+    zipcode: false,
+    dni: false,
+}
+
+function validation (e) {
+    e.preventDefault();
+    console.log(e.target.value);
+    switch (e.target.name){
+        case 'fullname':
+            validateInput(expressions.fullname, e.target, 'fullname');
+        break;
+        case 'email':
+            validateInput(expressions.email, e.target, 'email');
+        break;
+        case 'password':
+            validateInput(expressions.password, e.target, 'password');
+        break;
+        case 'repeatpass':
+            validatePassword();
+        break;
+        case 'age':
+            validateInput(expressions.age, e.target, 'age');
+        break;
+        case 'phone':
+            validateInput(expressions.phone, e.target, 'phone');
+        break;
+        case 'address':
+            validateInput(expressions.address, e.target, 'address');
+        break;
+        case 'city':
+            validateInput(expressions.city, e.target, 'city');
+        break;
+        case 'zipcode':
+            validateInput(expressions.zipcode, e.target, 'zipcode');
+        break;
+        case 'dni':
+            validateInput(expressions.dni, e.target, 'dni');
+        break;
+    }
+}
+
+function validateInput(expression,input,field){
+    if(expression.test(input.value)){
+        document.getElementById(`group-${field}`).classList.remove('wrong-group');
+        document.getElementById(`group-${field}`).classList.add('right-group');
+        document.querySelector(`#group-${field} .input-error`).classList.remove('input-error-active');
+        fields[field] = true;
+    }else{
+        document.getElementById(`group-${field}`).classList.add('wrong-group');
+        document.getElementById(`group-${field}`).classList.remove('right-group');
+        document.querySelector(`#group-${field} .input-error`).classList.add('input-error-active');
+        fields[field] = false;
+    }
+}
+
+input.forEach((input) => {
+    input.addEventListener('blur', validation);
+});
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+});
+
+function runEvent(e){
+    console.log('Event type: '+e.type);
+}
+
+
+
+
+function sendform(){
+    alert();
+    return false;
+}
